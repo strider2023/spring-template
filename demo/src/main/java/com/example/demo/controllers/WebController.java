71e9d1ea9c645c1c6c53f2f97241ab1a75b11d0d
Arindam Nath <arindam.nath@optimizeitsystems.com>
2020-07-07 16:35:57 +0530

    Updated rendering logic with react js

--- demo/src/main/java/com/example/demo/controllers/WebController.java
+++ demo/src/main/java/com/example/demo/controllers/WebController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
+    
     @RequestMapping(value = "/index")
     public String index() {
         return "index";
